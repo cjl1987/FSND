daf18006ac480a63ff43a35066a7fd0ff1d3a86d
Christoph <christoph.leichte@googlemail.com>
2020-06-14 23:09:41 +0200

    Added node_modules to .gitignore and applied code style aligned to PEP 8 (pycodestyle)

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -18,183 +18,172 @@ CORS(app)
 '''
 db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
 
-#GET endpoint to get drinks
+# GET endpoint to get drinks
+
+
 @app.route('/drinks', methods=['GET'])
 def get_drinks():
     try:
         drinks = []
 
-        #get drinks from data base
-        selection=Drink.query.all()
-        
-        #check whether selection is empty
+        # get drinks from data base
+        selection = Drink.query.all()
+        # check whether selection is empty
         if len(selection) == 0:
             abort(422)
-        
-        #return all drinks in short() form
+        # return all drinks in short() form
         for drinks_selected in selection:
             drinks.append(drinks_selected.short())
-        
         return jsonify({
-                        "success": True, 
+                        "success": True,
                         "drinks": drinks
                         }), 200
-    except: 
+    except Exception:
         abort(422)
 
 
-
-#GET endpoint to get drinks-detail
+# GET endpoint to get drinks-detail
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
 def get_drinks_detail(jwt):
     try:
         drinks = []
-
-        #get drinks from data base
-        selection=Drink.query.all()
-        
-        #check whether selection is empty
+        # get drinks from data base
+        selection = Drink.query.all()
+        # check whether selection is empty
         if len(selection) == 0:
-            abort(422)
-        
-        #return all drinks in long() form
+            abort(404)
+        # return all drinks in long() form
         for drinks_selected in selection:
             drinks.append(drinks_selected.long())
-        
         return jsonify({
-                        "success": True, 
+                        "success": True,
                         "drinks": drinks
                         }), 200
-    except: 
+    except Exception:
         abort(422)
 
 
-# POST /drinks  
-@app.route('/drinks', methods=['POST'])   
-@requires_auth('post:drinks')  
+# POST /drinks expects a body with 'title' and 'recipe'
+# and will returns {"success": True, "drinks": [drink.long()]}
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
 def create_drink(jwt):
-    #get json object
+    # get json object
     body = request.get_json()
     new_title = body.get('title', None)
     new_recipe = body.get('recipe', None)
 
-    #cast new recipe to list
+    # cast new recipe to list
     if isinstance(new_recipe, dict):
-        new_recipe = [new_recipe]   
+        new_recipe = [new_recipe]
 
-    #check whether user input is complete
-    if new_title == None:
-        abort(422) 
-    if new_recipe == None:
-        abort(422) 
+    # check whether user input is complete
+    if new_title is None:
+        abort(422)
+    if new_recipe is None:
+        abort(422)
 
     try:
-        #add row in data base
+        # add row in data base
         drink = Drink()
         drink.title = new_title
         drink.recipe = json.dumps(new_recipe)
         drink.insert()
 
-        #return json response          
+        # return json response
         return jsonify({
                         "success": True,
                         "drinks": [drink.long()]
-                        })
-    except:
+                        }), 201
+    except Exception:
         abort(422)
 
 
-
-#PATCH drink by provided drink_id
+# PATCH drink by provided drink_id
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def drinks_update(jwt, drink_id):
-    #load PATCH body
+    # load PATCH body
     body = request.get_json()
-
-    #get drink by id
+    # get drink by id
     try:
-        drink = Drink.query.filter(Drink.id==drink_id).one_or_none()
-       
-        #error 404
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        # error 404
         if drink is None:
             abort(404)
-
-        #prepare body
+        # prepare body
         if body.get('title'):
             drink.title = body.get('title')
         if body.get('recipe'):
             drink.recipe = json.dumps(body.get('recipe'))
-        
-        #update data base
+        # update data base
         drink.update()
         return jsonify({
-                        "success": True, 
+                        "success": True,
                         "drinks": [drink.long()]
                         }), 200
-    except:
+    except Exception:
         abort(422)
 
 
-
-#Delete drink
+# delete drink /drinks/<int:drink_id>
+# returns {'success' : True, 'delete': drink_id})
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drink(jwt, drink_id):
-    
-    try: 
-        #get drink by drink_id
-        drink = Drink.query.filter(Drink.id==drink_id).one_or_none() 
-
-        #error 404
+    try:
+        # get drink by drink_id
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        # error 404
         if drink is None:
             abort(404)
-
-        #delete row in data base
+        # delete row in data base
         drink.delete()
-   
         return jsonify({
-                        'success' : True, 
+                        'success': True,
                         'delete': drink_id
                         }), 200
-
-    except: 
+    except Exception:
         abort(422)
 
 
-## Error Handling
+# Error Handling
 
-#Error-Handler 422
+
+# Error-Handler 422
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
 
-#Error-Handler 404
+
+# Error-Handler 404
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({
                     "success": False,
-                    "error": 404, 
+                    "error": 404,
                     "message": "resource not found"
                     }), 404
 
-#Error-Handler 400
+
+# Error-Handler 400
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 400,
                     "message": "bad request"
                     }), 400
 
-#Error-Handler AuthErrors
+
+# Error-Handler AuthErrors
 @app.errorhandler(AuthError)
 def auth_error(error):
     return jsonify({
@@ -202,4 +191,3 @@ def auth_error(error):
         "error": error.status_code,
         "message": error.error['description']
     }), error.status_code
-
